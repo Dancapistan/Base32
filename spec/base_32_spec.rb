4c974e1b82ce074269db1df9eac95bc7a8019fe3
Daniel Royer <daniel.royer@gmail.com>
2012-02-12 16:50:20 -0500

    Added some mathematical tests

--- spec/base_32_spec.rb
+++ spec/base_32_spec.rb
@@ -16,124 +16,140 @@ describe Base32 do
   
   context 'decode' do
     
-    it 'decodes 0_32 to be 0_10' do
+    it 'decodes 0 to be 0' do
       Base32.decode('0').should == 0
     end
     
-    it 'decodes 1_32 to be 1_10' do
+    it 'decodes 1 to be 1' do
       Base32.decode('1').should == 1
     end
     
-    it 'decodes 2_32 to be 2_10' do
+    it 'decodes 2 to be 2' do
       Base32.decode('2').should == 2
     end
     
-    it 'decodes 3_32 to be 3_10' do
+    it 'decodes 3 to be 3' do
       Base32.decode('3').should == 3
     end
     
-    it 'decodes 9_32 to be 9_10' do
+    it 'decodes 9 to be 9' do
       Base32.decode('9').should == 9
     end
     
-    it 'decodes a_32 to be 10_10' do
+    it 'decodes a to be 10' do
       Base32.decode('a').should == 10
     end
     
-    it 'decodes B_32 to be 11_10' do
+    it 'decodes B to be 11' do
       Base32.decode('B').should == 11
     end
     
-    it 'decodes c_32 to be 12_10' do
+    it 'decodes c to be 12' do
       Base32.decode('c').should == 12
     end
     
-    it 'decodes Z_32 to be 31_10' do
+    it 'decodes Z to be 31' do
       Base32.decode('Z').should == 31
     end
     
-    it 'decodes 10_32 to be 32_10' do
+    it 'decodes 10 to be 32' do
       Base32.decode('10').should == 32
     end
     
-    it 'decodes 11_32 to be 33_10' do
+    it 'decodes 11 to be 33' do
       Base32.decode('11').should == 33
     end
     
-    it 'decodes il_32 to be 33_10' do
+    it 'decodes il to be 33' do
       Base32.decode('il').should == 33
     end
     
-    it 'decodes 1234ABCD_32 to be 36612418957_10' do
+    it 'decodes 1234ABCD to be 36612418957' do
       Base32.decode('1234ABCD').should == 36612418957
     end
     
-    it 'decodes ZEND_32 to be 1030829_10' do
+    it 'decodes ZEND to be 1030829' do
       Base32.decode('ZEND').should == 1030829
     end
     
-    it 'decodes DaNIEl_32 to be 447382977_10' do
+    it 'decodes DaNIEl to be 447382977' do
       Base32.decode('DaNIEl').should == 447382977
+    end
+    
+    it 'decodes ZZZZZ to be 32^5-1' do
+      Base32.decode('ZZZZZ').should == (32**5 - 1)
+    end
+    
+    it 'decodes ZZZZZZZZZZZ to be 32^11-1' do
+      Base32.decode('ZZZZZZZZZZZ').should == (32**11 - 1)
     end
     
   end
   
   context 'encode' do
     
-    it 'encodes 0_10 to be 0_32' do
+    it 'encodes 0 to be 0' do
       Base32.encode(0).should == '0'
     end
     
-    it 'encodes 1_10 to be 1_32' do
+    it 'encodes 1 to be 1' do
       Base32.encode(1).should == '1'
     end
     
-    it 'encodes 2_10 to be 2_32' do
+    it 'encodes 2 to be 2' do
       Base32.encode(2).should == '2'
     end
     
-    it 'encodes 3_10 to be 3_32' do
+    it 'encodes 3 to be 3' do
       Base32.encode(3).should == '3'
     end
     
-    it 'encodes 9_10 to be 9_32' do
+    it 'encodes 9 to be 9' do
       Base32.encode(9).should == '9'
     end
     
-    it 'encodes 10_10 to be a_32' do
+    it 'encodes 10 to be a' do
       Base32.encode(10).should == 'A'
     end
     
-    it 'encodes 11_10 to be B_32' do
+    it 'encodes 11 to be B' do
       Base32.encode(11).should == 'B'
     end
     
-    it 'encodes 12_10 to be c_32' do
+    it 'encodes 12 to be c' do
       Base32.encode(12).should == 'C'
     end
     
-    it 'encodes 31_10 to be Z_32' do
+    it 'encodes 31 to be Z' do
       Base32.encode(31).should == 'Z'
     end
     
-    it 'encodes 32_10 to be 10_32' do
+    it 'encodes 32 to be 10' do
       Base32.encode(32).should == '10'
     end
     
-    it 'encodes 33_10 to be 11_32' do
+    it 'encodes 33 to be 11' do
       Base32.encode(33).should == '11'
     end
         
-    it 'encodes 36612418957_10 to be 1234ABCD_32' do
+    it 'encodes 36612418957 to be 1234ABCD' do
       Base32.encode(36612418957).should == '1234ABCD'
     end
     
-    it 'encodes 1030829_10 to be ZEND_32' do
+    it 'encodes 1030829 to be ZEND' do
       Base32.encode(1030829).should == 'ZEND'
     end
     
-    it 'encodes 447382977_10 to be DAN1E1_32' do
+    it 'encodes 447382977 to be DAN1E1' do
       Base32.encode(447382977).should == 'DAN1E1'
+    end
+    
+    it 'encodes 32^5-1 to be ZZZZZ' do
+      Base32.encode(32**5-1).should == 'ZZZZZ'
+    end
+    
+    it 'encodes 32^11-1 to be ZZZZZZZZZZZ' do
+      Base32.encode(32**11 - 1).should == 'ZZZZZZZZZZZ'
     end
     
   end
